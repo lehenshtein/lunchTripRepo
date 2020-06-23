425b962381413afc8726cde4043b3892fe7c5188
lehenshtein <lehenshtein@gmail.com>
2020-06-23 17:11:40 +0300

    BACK&FRONT api error handling (#2)
    * [Back] Global error exception handler added
    
    * [Front] Error interceptor added

--- DatingApp-SPA/src/app/default/default.component.ts
+++ DatingApp-SPA/src/app/default/default.component.ts
@@ -34,15 +34,27 @@ export class DefaultComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  public sum(arr: Array<number>): number { // суммирование элементов массива, при каждой
+    if (arr.length <= 0) {return 0; }         // итерации, которого, кол-во элементов массива уменьшается на 1
+    if (arr.length === 1) { return arr[0]; }
+    else {
+      const firstEl = arr[0];
+      arr.shift();
+      return firstEl + this.sum(arr);
+    }
+  }
 
   public arrayActions() {
+    console.log('Quick sort: ', this.sortingService.qsort(this.list));
+    console.log('badQsort sort: ', this.sortingService.badQsort([6,3,8,5,2,9,5,23,76,11,87,5]));
+
     this.list.sort((a, b) => a - b);
     const newStringArray = this.stringList.map(el => el.toLowerCase()).sort();
     console.log(this.list);
     console.log(newStringArray);
     console.log('Binary search index: ', this.binarySearchService.search(this.list, 31));
     console.log('Recursive search index: ', this.binarySearchService.recursiveSearch(newStringArray, 'f'));
-    console.log('Array sum with removing first array el: ', this.sortingService.sum([1, 2, 3]));
+    console.log('Array sum with removing first array el: ', this.sum([1, 2, 3]));
     console.log('Get linked list elements: ',this.linkedListService.getLinkedListLength(this.linkedList));
   }
 
---
+++ DatingApp-SPA/src/app/shared/interceptors/error.interceptor.ts
@@ -0,0 +1,46 @@
+import {Injectable} from "@angular/core";
+import {
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError( err => {
+        if(err.status === 401) {
+          return throwError(err.statusText);
+        }
+
+        if(err instanceof HttpErrorResponse) { // 50* errors
+          const applicationError = err.headers.get('Application-Error');
+          if (applicationError) {
+            return throwError(applicationError);
+          }
+          const serverError = err.error;
+          let modalStateErrors = '';
+          if(serverError.errors && typeof serverError.errors === 'object') {
+            for (const key in serverError.errors) {
+              if(serverError.errors.hasOwnProperty(key)) {
+                modalStateErrors += serverError.errors[key] + '\n';
+              }
+            }
+          }
+          return throwError(modalStateErrors || serverError || 'Server error');
+        }
+      })
+    )
+  }
+}
+export const ErrorInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
+}
